7717af889770ea51de114821bf640e2d6e36d028
snow-0122 <suzu1003m44@gmail.com>
2025-03-30 15:04:14 +0900

    ログイン画面のレイアウト

--- AtlasManagementSystem_Suzunaaida/resources/views/auth/login/login.blade.php
+++ AtlasManagementSystem_Suzunaaida/resources/views/auth/login/login.blade.php
@@ -1,30 +1,39 @@
 <x-guest-layout>
-  <form action="{{ route('login') }}" method="POST">
-    <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
-      <div class="border vh-50 w-25">
-        <div class="w-75 m-auto pt-5">
-          <label class="d-block m-0" style="font-size:13px;">メールアドレス</label>
-          <div class="border-bottom border-primary w-100">
-            <input type="text" class="w-100 border-0" name="mail_address">
-          </div>
-        </div>
-        <div class="w-75 m-auto pt-5">
-          <label class="d-block m-0" style="font-size:13px;">パスワード</label>
-          <div class="border-bottom border-primary w-100">
-            <input type="password" class="w-100 border-0" name="password">
-          </div>
-        </div>
-        <div class="text-right m-3">
-          <input type="submit" class="btn btn-primary" value="ログイン">
-        </div>
-        <div class="text-center">
-          <a href="{{ route('registerView') }}">新規登録</a>
-        </div>
-      </div>
-      {{ csrf_field() }}
+  <div class="login-page">
+    {{-- ロゴ --}}
+    <div class="logo-box">
+      <img src="{{ asset('image/atlas-black.png') }}" alt="Atlas ロゴ" class="login-logo">
     </div>
-  </form>
+
+    {{-- ログインフォーム --}}
+    <form action="{{ route('login') }}" method="POST" class="login-form-box">
+      @csrf
+
+      {{-- メールアドレス --}}
+      <div class="mb-4">
+        <label class="d-block m-0" style="font-size:13px;">メールアドレス</label>
+        <input type="text" name="mail_address" class="login-input">
+      </div>
+
+      {{-- パスワード --}}
+      <div class="mb-4">
+        <label class="d-block m-0" style="font-size:13px;">パスワード</label>
+        <input type="password" name="password" class="login-input">
+      </div>
+
+      {{-- ログインボタン --}}
+      <div class="text-right m-3">
+        <input type="submit" class="btn btn-primary" value="ログイン">
+      </div>
+
+
+      {{-- 新規登録リンク --}}
+      <div class="text-center">
+        <a href="{{ route('registerView') }}" class="text-primary" style="font-size: 14px;">新規登録はこちら</a>
+      </div>
+    </form>
   </div>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  <script src="{{ asset('js/register.js') }}" rel="stylesheet"></script>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+  <script src="{{ asset('js/register.js') }}"></script>
 </x-guest-layout>
--- AtlasManagementSystem_Suzunaaida/resources/views/authenticated/calendar/general/calendar.blade.php
+++ AtlasManagementSystem_Suzunaaida/resources/views/authenticated/calendar/general/calendar.blade.php
@@ -3,11 +3,34 @@
     <div class="border w-75 m-auto pt-5 pb-5" style="border-radius:5px; background:#FFF;">
       <div class="w-75 m-auto border" style="border-radius:5px;">
 
+        <!-- カレンダータイトル -->
         <p class="text-center">{{ $calendar->getTitle() }}</p>
+
+        <!-- カレンダー描画 -->
         <div class="">
           {!! $calendar->render() !!}
+
+          <!-- キャンセル確認モーダル -->
+          <div class="modal" id="cancelModal" tabindex="-1" style="display: none; position: fixed; z-index: 9999; top: 30%; left: 50%; transform: translate(-50%, -30%); background: white; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.2);">
+            <div class="modal-content">
+              <h5>予約キャンセル確認</h5>
+              <p>予約日：<span id="modal-date"></span></p>
+              <p>予約時間：<span id="modal-time"></span></p>
+              <p>上記の予約をキャンセルしてよろしいですか？</p>
+
+              <form id="cancel-form" method="POST" action="{{ route('deleteParts') }}">
+                @csrf
+                <input type="hidden" name="date" id="input-date">
+                <input type="hidden" name="part" id="input-part">
+                <button type="submit" class="btn btn-danger">キャンセルする</button>
+                <button type="button" class="btn btn-secondary close-modal">閉じる</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
+
+      <!-- 予約ボタン -->
       <div class="text-right w-75 m-auto">
         <input type="submit" class="btn btn-primary" value="予約する" form="reserveParts">
       </div>
